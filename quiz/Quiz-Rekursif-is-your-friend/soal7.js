41ddb54c32cb19f2d9daaaf5511e50fc410a4db5
rsiddik17 <rsiddik369@gmail.com>
2026-04-02 12:00:13 +0700

    rekursif-is-your-friend done

---
+++ quiz/Quiz-Rekursif-is-your-friend/soal7.js
@@ -0,0 +1,33 @@
+/*
+  PARSE NUMBER
+  Parse Number adalah sebuah fungsi untuk memecah atau menguraikan suatu angka. Fungsi akan
+  menerima parameter berupa angka dan keluaran berupa string uraian angka.
+
+  EXAMPLE
+  INPUT: 4321
+  OUTPUT: 4000+300+20+1
+
+  RULES:
+  1. Wajib menggunakan rekursif!
+  2. Tidak boleh menambahkan parameter dan function baru
+*/
+
+function parseNumber(equation) {
+  //code here
+  let str = String(equation);
+
+  if (str.length < 2) return str;
+
+  let rest = Number(str.slice(1));
+
+  if(rest === 0) return str;
+
+  let result = str[0] + "0".repeat(str.length - 1) + "+";
+
+  return result + parseNumber(Number(str.slice(1)));
+
+};
+
+console.log(parseNumber(3333)) // 3000+300+30+3  // 3000 + 300 + 30 + 3
+console.log(parseNumber(90)) // 90
+console.log(parseNumber(2333)) // 2000+300+30+3
